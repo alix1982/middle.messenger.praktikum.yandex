355cc291a782cf3f00532dc7aa1c67dca0bc2eee
Alix1982 <alix1982@yandex.ru>
2025-08-27 21:23:18 +0300

    fix_err_test_pr7

--- src/index.js
+++ src/index.js
@@ -37,39 +37,4 @@ export function render() {
     }
 }
 
-render()
-
-// console.log(window.location.pathname)
-// if (window.location.pathname.includes('login')) {
-//     loginHtml();
-// } else if (window.location.pathname.includes('registration')) {
-//     registrationHtml()
-// } else if (window.location.pathname.includes('profile')) {
-//     profileHtml()
-// } else if (window.location.pathname.includes('chats')) {
-//     chatsHtml()
-// } else if (window.location.pathname.includes('errorRout404')) {
-//     errorRout404Html()
-// } else if (window.location.pathname.includes('errorRout500')) {
-//     errorRout500Html()
-// } else {
-//     loginHtml();
-// }
-
-// function load() {
-//     console.log(window.location.href)
-//     if (window.location.pathname.includes('errorRout404')) {
-//         window.location.href = 'errorRout404.html'
-//     } else if (window.location.pathname.includes('errorRout500')) {
-//         window.location.href = 'errorRout500.html'
-//     } 
-    // else if (window.location.pathname.includes('chats')) {
-    //     window.location.href = 'chats.html'
-    // } else if (window.location.pathname.includes('profile')) {
-    //     window.location.href = 'profile.html'
-    // } else if (window.location.pathname.includes('registration')) {
-    //     window.location.href = 'registration.html'
-    // }
-
-// document.addEventListener('DOMContentLoaded', () => {load()}, true);
-//  document.removeEventListener('DOMContentLoaded', () => {load()}, true)
+render();
