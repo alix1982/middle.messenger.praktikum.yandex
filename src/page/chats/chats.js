0866a0c0b26bd7e52a76cf3a95e190204005ecb6
Alix1982 <alix1982@yandex.ru>
2025-08-27 21:26:46 +0300

    fix_err_test_pr8

--- src/page/chats/chats.js
+++ src/page/chats/chats.js
@@ -16,19 +16,3 @@ export default function (dataChats, idChatSelected) {
         </section>
     `
 }
-
-// export default function (dataChats, idChatSelected) {
-//     // console.log(idChatSelected)
-//     return `
-//          <section class='chats'>
-//             <div class='chats__list'>
-//                 <a class='chats__profile' href='../../profile.html'>Профиль &gt;</a>
-//                 ${input('search', 'text', '&#128269;&nbsp;Поиск ')}
-//                 ${chatsList(dataChats)}
-//             </div>
-//             <div class='chats__item'>
-//                 <p class='chats__itemText'>Выберите чат чтобы отправить сообщение</p>
-//             </div>
-//         </section>
-//     `
-// }
